f79a3409cb1efb383ec8b26c414013794658a6a6
AvraamNikolaou <avnikolaou@hotmail.com>
2020-11-04 16:42:18 +0200

    refactor: social links and style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons'
+
 const Footer = () => {
     return (
         <footer>
-            footer
+            <div className={'footer-icons'}>
+                <a href={'https://www.linkedin.com/in/avraam-nikolaou/'} target={'_blank'}><FontAwesomeIcon icon={faLinkedinIn}/></a>
+
+                <a href={'https://github.com/avnikolaou'} target={'_blank'}><FontAwesomeIcon icon={faGithub}/></a>
+            </div>
+
+            <div className={'footer-copyright'}>
+                <span>Designed and developed with love | {(new Date().getFullYear())}</span>
+            </div>
         </footer>
     )
 }
