8a2e71d52ddb7cee772ccb90a12c98bcb73ca874
AvraamNikolaou <avnikolaou@hotmail.com>
2020-11-04 12:28:51 +0200

    feat: personal info icons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home';
 
 function App() {
     return (
         <div>
             <Navbar />
+            <Switch>
+                <Route exact={'/'} component={Home} />
+            </Switch>
         </div>
     );
 }
