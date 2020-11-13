f800c061eaf24c5487e15497b45292a9e7a4c63d
AvraamNikolaou <avnikolaou@hotmail.com>
2020-11-13 12:42:39 +0200

    feat: validation for fields

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,17 +7,45 @@ const Contact = () => {
         document.title = 'Contact - Avraam Nikolaou'
     });
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+    const initialValues = {
+        name: '',
+        email: '',
+        message: ''
+    }
+
+    const onSubmit = (values) => {
+        console.log('Form data: ', values)
+    }
+
+    const validate = (values) => {
+        let errors = {
+
+        }
+
+        if (!values.name) {
+            errors.name = 'Required'
+        }
+
+        if (!values.email) {
+            errors.email = 'Required'
+        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+            errors.email = 'Invalid  email format'
+        }
+
+        if (!values.message) {
+            errors.message = 'Required'
+        }
+
+        return errors
     }
 
     const formik = useFormik({
-        initialValues: {
-            name: '',
-            email: '',
-            message: ''
-        }
+        initialValues,
+        onSubmit,
+        validate
     });
+
+    console.log('Formik errors: ', formik.errors)
 
     return (
         <div className={'contact-container'}>
@@ -28,34 +56,43 @@ const Contact = () => {
             </div>
 
             <div className={'form'}>
-                <form className={'submission-form'}>
-                    <label htmlFor={'name'}>Name</label>
-                    <input
-                        type="text"
-                        id={'name'}
-                        name={'name'}
-                        value={formik.values.name}
-                        onChange={formik.handleChange}
-                    />
+                <form className={'submission-form'} onSubmit={formik.handleSubmit}>
+                    <div className={'form-control'}>
+                        <label htmlFor={'name'}>Name</label>
+                        <input
+                            type="text"
+                            id={'name'}
+                            name={'name'}
+                            value={formik.values.name}
+                            onChange={formik.handleChange}
+                        />
+                        {formik.errors.name ? <div className={'error'}>{formik.errors.name}</div> : null}
+                    </div>
 
-                    <label htmlFor={'email'}>Email</label>
-                    <input
-                        type={'email'}
-                        id={'email'}
-                        name={'email'}
-                        value={formik.values.email}
-                        onChange={formik.handleChange}
-                    />
+                    <div className={'form-control'}>
+                        <label htmlFor={'email'}>Email</label>
+                        <input
+                            type={'text'}
+                            id={'email'}
+                            name={'email'}
+                            value={formik.values.email}
+                            onChange={formik.handleChange}
+                        />
+                        {formik.errors.email ? <div className={'error'}>{formik.errors.email}</div> : null}
+                    </div>
 
-                    <label htmlFor={'message'}>Message</label>
-                    <textarea
-                        id={'message'}
-                        name={'message'}
-                        rows="5"
-                        value={formik.values.message} onChange={formik.handleChange}
-                    />
+                    <div className={'form-control'}>
+                        <label htmlFor={'message'}>Message</label>
+                        <textarea
+                            id={'message'}
+                            name={'message'}
+                            rows='5'
+                            value={formik.values.message} onChange={formik.handleChange}
+                        />
+                        {formik.errors.message ? <div className={'error'}>{formik.errors.message}</div> : null}
+                    </div>
 
-                    <input className={'sendButton'} type={'submit'} value={'Send'} onClick={handleSubmit}/>
+                    <button className={'sendButton'} type={'submit'} value={'Send'}>Send</button>
                 </form>
             </div>
 
